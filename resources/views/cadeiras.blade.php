94a4b9056f4924b45f685c1b0e56a1146df4d260
Rafael Cursos <114831366+rafaelcursos@users.noreply.github.com>
2022-10-15 15:03:47 -0300

    mudanças de dimencionamento e inserção das cadeiras

--- resources/views/cadeiras.blade.php
+++ resources/views/cadeiras.blade.php
@@ -4,17 +4,25 @@
 <div class="row">
     <div class="col-lg-8">
         <h3>Monte Seu Móvel!</h3>
-        <h4 class="m-0" id="c-t-titulo">Escolha o Tampo</h4><br>
-        <h4 class="m-0" id="c-b-titulo">Escolha a Base</h4>
-        <img id="c-t-img" src="https://via.placeholder.com/1280x300?text=img-tampo" alt="tampo" class="img-fluid" />
-        <img id="c-b-img" src="https://via.placeholder.com/1280x300?text=img-base" alt="base" class="img-fluid" />
+        <h4 class="m-0" id="c-t-titulo">...</h4><br>
+        <h4 class="m-0" id="c-b-titulo">...</h4>
+        <div class="row c-mesa">
+            <img id="c-t-img" src="https://via.placeholder.com/1280x300?text=img-tampo" alt="tampo" class="img-fluid" />
+            <img id="c-b-img" src="https://via.placeholder.com/1280x300?text=img-base" alt="base" class="img-fluid" />
+        </div>
+        <div class="row c-cadeira">
+            <div class="col-9 m-auto">
+                <img id="c-c-img" src="img/cad-gaya-costas.png" alt="tampo" class="img-fluid frente" />
+            </div>
+        </div>
+
     </div>
-    <div class="col-lg-4">
+    <div class="col-lg-4 area-cadeira">
         <div class="row">
             <div class="col-sm-12 col-md-6 col-lg-12">
                 <h3 class="p-2">Cadeiras</h3>
                 <button id="opentampos" class="btn btn-primary m-1">Cadeiras</button><br>
-                <img src="https://via.placeholder.com/300?text=cadeira" alt="mesa com cadeira" class="img-fluid" /><br>
+                <img id="c-c-img" src="img/cad-gaya-frent.png" alt="tampo" class="img-fluid" />
                 <button id="openvidros" class="btn btn-primary m-1">Tecido</button>
             </div>
             <div class="col-sm-12 col-md-6 col-lg-12">
